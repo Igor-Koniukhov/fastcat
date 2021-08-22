70563be576529bb1112fcd0d1a308091b774f4cb
Igor Koniukhov <ikoniukhov@gmail.com>
2021-08-22 22:20:37 +0300

    added necessary fields in the structure Product of pkg models

--- internal/model/product.go
+++ internal/model/product.go
@@ -1,9 +1,14 @@
 package model
 
 type Product struct {
-	ID           int32   `json:"id"`
+	ID           int     `json:"id"`
+	SupplierID   int     `json:"supplier_id"`
 	Name         string  `json:"name"`
 	Description  string  `json:"description"`
 	ImgReference string  `json:"img_reference"`
 	Price        float64 `json:"price"`
+	Status       string  `json:"status"`
+	CreatedAt    string  `json:"created_at"`
+	UpdatedAt    string  `json:"updated_at"`
 }
+
