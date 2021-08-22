408059069c9761397c933e6aae159a8821c2db46
Igor Koniukhov <ikoniukhov@gmail.com>
2021-08-22 22:08:58 +0300

    added necessary fields in the structure of pkg models

--- internal/model/cart.go
+++ internal/model/cart.go
@@ -1,7 +1,18 @@
 package model
 
-type Cart struct{
-	ID int32 `json:"id"`
-	Items []Product `json:"items"`
+type Cart struct {
+	ID        int            `json:"id"`
+	UserID    int            `json:"user_id"`
+	ProductID int            `json:"product_id"`
+	Items     []CartProducts `json:"items"`
+	Status    string         `json:"status"`
+	CreatedAt string         `json:"created_at"`
+	UpdatedAt string         `json:"updated_at"`
 }
 
+type CartProducts struct {
+	ID        int `json:"id"`
+	CartID    int `json:"cart_id"`
+	ProductID int `json:"product_id"`
+	Number    int `json:"number"`
+}
