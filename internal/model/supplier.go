a494b197e31423ef5f2da37475884e2cf21aefde
Igor Koniukhov <ikoniukhov@gmail.com>
2021-08-22 22:23:02 +0300

    added necessary fields in the structure Supplier of pkg models

--- internal/model/supplier.go
+++ internal/model/supplier.go
@@ -1,12 +1,14 @@
 package model
 
 type Supplier struct {
-	ID          int32  `json:"id"`
+	ID          int    `json:"id"`
 	Category    string `json:"category"`
 	Name        string `json:"name"`
-	AddressName string `json:"addressName"`
+	Address     string `json:"addressName"`
 	Description string `json:"description"`
 	IconURL     string `json:"icon_url"`
 	Email       string `json:"email"`
 	PhoneNumber string `json:"phone_number"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
 }
