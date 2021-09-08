6f4c4188a4d95f2b1394aab3bec6c32f820ab89d
Igor Koniukhov <ikoniukhov@gmail.com>
2021-09-08 11:03:22 +0300

    set parameters and conditions for gracefully shuts down server without interupting any active connections

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,17 @@
 package main
 
 import (
+	"context"
 	_ "github.com/go-sql-driver/mysql"
-	"github.com/igor-koniukhov/fastcat/controllers"
 	"github.com/igor-koniukhov/fastcat/driver"
 	"github.com/igor-koniukhov/fastcat/internal/config"
-	"github.com/igor-koniukhov/fastcat/internal/repository"
+	"github.com/igor-koniukhov/fastcat/internal/server"
+	web "github.com/igor-koniukhov/webLogger/v3"
 	"github.com/subosito/gotenv"
-	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 var app config.AppConfig
@@ -27,41 +29,23 @@ func main() {
 	SetWebLoggerParameters()
 	go RunUpToDateSuppliersInfo(600)
 
-	repo := repository.NewRepository(&app)
-	c := controllers.NewControllers(repo)
-	controllers.NewController(c)
-	repository.NewRepo(repo)
-
-	http.HandleFunc("/user/create", c.User.Create())
-	http.HandleFunc("/user/", c.User.Get())
-	http.HandleFunc("/users", c.User.GetAll())
-	http.HandleFunc("/user/update/", c.User.Update())
-	http.HandleFunc("/user/delete/", c.User.Delete())
-
-	http.HandleFunc("/order/create", c.Order.Create())
-	http.HandleFunc("/order/", c.Order.Get())
-	http.HandleFunc("/orders", c.Order.GetAll())
-	http.HandleFunc("/order/update/", c.Order.Update())
-	http.HandleFunc("/order/delete/", c.Order.Delete())
-
-	http.HandleFunc("/supplier/create", c.Supplier.Create())
-	http.HandleFunc("/supplier/", c.Supplier.Get())
-	http.HandleFunc("/suppliers", c.Supplier.GetAll())
-	http.HandleFunc("/supplier/update/", c.Supplier.Update())
-	http.HandleFunc("/supplier/delete/", c.Supplier.Delete())
-
-	http.HandleFunc("/product/create", c.Product.Update())
-	http.HandleFunc("/product/", c.Product.Get())
-	http.HandleFunc("/products", c.Product.GetAll())
-	http.HandleFunc("/product/update/", c.Product.Update())
-	http.HandleFunc("/product/delete/", c.Product.Delete())
-
-	http.HandleFunc("/cart/create", c.Cart.Create())
-	http.HandleFunc("/cart/", c.Cart.Get())
-	http.HandleFunc("/cart", c.Cart.GetAll())
-	http.HandleFunc("/cart/update/", c.Cart.Update())
-	http.HandleFunc("/cart/delete/", c.Cart.Delete())
-
 	http.Handle("/", http.FileServer(http.Dir("./public")))
-	log.Fatal(http.ListenAndServe(port, nil))
+	srv := new(server.Server)
+	go func() {
+		err := srv.Serve(port, routes(&app))
+		web.Log.Fatal(err, err, " got an error while running http server")
+	}()
+
+	web.Log.Info("FastCat application Started")
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	<-c
+
+	web.Log.Info("FastCat application Shutting Down")
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	err := srv.Shutdown(ctx)
+	web.Log.Error(err, err, "got an error on DB connection close")
 }
+
